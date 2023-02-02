307d0e48499d8fa2e949bab0b2c970317e366874
Allison Ang <allisonang21@gmail.com>
2023-02-01 20:10:23 -0800

    adding table relations

--- models/Product.js
+++ models/Product.js
@@ -26,6 +26,20 @@ Product.init(
         isDecimal: true,
       },
     },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
