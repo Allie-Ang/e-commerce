859cc28ec02b47d3a204000572dee7e8fd3f5369
Allison Ang <allisonang21@gmail.com>
2023-02-02 16:33:25 -0800

    adding put route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,8 +36,21 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.body.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category associated with this id" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
