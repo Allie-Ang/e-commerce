06f6fd06b1ef494a428e63e3e743496c3fc1ee31
Allison Ang <allisonang21@gmail.com>
2023-02-03 15:52:15 -0800

    adding tag delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,8 +62,23 @@ router.put("/:id", async (req, res) => {
 });
 
 //* delete on tag by its `id` value
-router.delete("/:id", (req, res) => {
-  
+router.delete("/:id", async (req, res) => {
+  try {
+    const tagData = await Tag.destray({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({
+        message: "No tag found with this ID",
+      });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
